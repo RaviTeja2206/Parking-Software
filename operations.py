ce6a1383c274e59ca5416789d5c9598de2c95328
RaviTeja2206 <103409351+RaviTeja2206@users.noreply.github.com>
2023-03-02 19:29:53 +0530

    Add files via upload

--- operations.py
+++ operations.py
@@ -1,11 +1,10 @@
 def software(park_buliding):
-    print("""
-      Choose one Option from below
-      1.Park the vehicle
-      2.Get the vehicle
-      3.Available Slots
-      4.View all slots
-      5.Exit
+    print("""Choose one Option from below
+    1.Park the vehicle
+    2.Get the vehicle
+    3.Available Slots
+    4.View all slots
+    5.Exit
     """)
     def slotsavailable(prnt):
       cnt=0
@@ -27,63 +26,33 @@ def software(park_buliding):
               print(f"floor{indx+1} -{x} ")
               prt="{}-{}".format(indx+1,a_indx+1)
               return prt
-              continue
       else:
         return "slots are not available"
-
-    def leave(parktkt):
+    def leave():
+      parktkt=input("Enter your parking ticket")
       tkt=parktkt.split('-')
-      floor=int(tkt[0])
-      slot=int(tkt[1])
+      floor,slot=int(tkt[0]),int(tkt[1])
       if 1<=floor<=len(park_buliding) and 1<=slot<=len(park_buliding[0]):
-        if park_buliding[int(tkt[0])-1][int(tkt[1])-1]=="O":
-          park_buliding[int(tkt[0])-1][int(tkt[1])-1]="A"
-          return park_buliding[int(tkt[0])-1] 
+        if park_buliding[floor-1][slot-1]=="O":
+          park_buliding[floor-1][slot-1]="A"
+          return park_buliding[floor-1] 
         else:
           return "Wrong Parking Ticket"
       else:
         return "Enter valid Parking Ticket"
-    
     def save():
       import csv
       with open('my_file.csv',mode='w') as file:
         writer=csv.writer(file)
-        a=["slot"+str(i+1) for i in range(len(park_buliding[0]))]
-        writer.writerow(a)
+        writer.writerow(["slot"+str(i+1) for i in range(len(park_buliding[0]))])
         for i in park_buliding:
           writer.writerow(i)  
     def view():
       import pandas as pd
-      # data=pd.read_csv("my_file.csv")
-      a=["slot"+str(i+1) for i in range(len(park_buliding[0]))]
-      index_labels=["floor"+str(i+1) for i in range(len(park_buliding))]
-      data=pd.DataFrame(park_buliding,columns=a,index=index_labels)
-      print(data.head())  
-             
-            
+      colum_name,index_labels=["slot"+str(i+1) for i in range(len(park_buliding[0]))],["floor"+str(i+1) for i in range(len(park_buliding))]
+      data=pd.DataFrame(park_buliding,columns=colum_name,index=index_labels)
+      print(data.head()) 
     option=int(input("You response: "))
-    if option>5 or option<=0:
-      print()
-      print("Your Response Should be 1 to 4")
-      software(park_buliding)
-    if option==1:
-      print()
-      print(f"your token :: {park()}")
-      software(park_buliding)
-    if option==2:
-      p_tkt=input("Enter your parking ticket")
-      print()
-      print(leave(p_tkt))
-      software(park_buliding)
-    if option==3:
-      print()
-      print("TotalSlots ::",slotsavailable(True))
-      software(park_buliding)
-    if option==4:
-      view()
-      software(park_buliding)
-    if option==5:
-      save()
-      print("exiting from the software")
-      return None
-    
+    op_dict={1:'print(f"your token :: {park()}")&software(park_buliding)',2:'print(leave())&software(park_buliding)',3:'print("TotalSlots ::",slotsavailable(True))&software(park_buliding)',4:'view()&software(park_buliding)',5:'save()&print("ExitingFromTheSoftware")'}
+    for i in op_dict[option].split('&'):
+      eval(i)
